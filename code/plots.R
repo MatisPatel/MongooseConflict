a2202d70f38fdaed2a25415a405254d3c06ebdcb
MatisPatel <matis.patel@gmail.com>
2021-05-13 15:33:10 +0100

    update.sh

--- code/plots.R
+++ code/plots.R
@@ -36,42 +36,78 @@ plotDat <- filter(dat,
     iX = mean(indAvgX),
     gY = mean(groupAvgY),
     iY = mean(indAvgY),
+    AgX = mean(AgroupAvgX),
+    AiX = mean(AindAvgX),
+    AgY = mean(AgroupAvgY),
+    AiY = mean(AindAvgY),
+    SgX = mean(SgroupAvgX),
+    SiX = mean(SindAvgX),
+    SgY = mean(SgroupAvgY),
+    SiY = mean(SindAvgY),
     avgR = mean(avgR)
   ) %>%
   mutate(
     investGY = gY/(gX+gY),
+    investGYA = AgY/(AgX+AgY),
+    investGYS = SgY/(SgX+SgY),
     investGX = gX/(gX+gY),
     investIY = iY/(iX+iY),
-    totalG = gY+gX
+    totalG = gY+gX,
+    totalGA = (AgX+AgY),
+    totalGA = (SgX+SgY)
     )%>%
   ungroup()
 
-# soical Y
-ggplot(filter(plotDat, tn==2)) +
+#total Y
+ggplot(filter(plotDat)) +
   facet_grid(~epsilon, scales="free") +
-  geom_path(aes(avgR, investGY,  color=treat), size=1) +
+  geom_path(aes(avgR, investGY,  color=as.factor(treat)), size=1) +
+  scale_colour_brewer(palette = "Dark2") +
+  my_theme
+
+ggplot(plotDat) +
+  facet_grid(~treat, scales="free") +
+  geom_point(aes((epsilon), gY/(mean(gY))), colour="green")+
+  geom_point(aes((epsilon), gX/mean(gX))) +
+  geom_line(aes(epsilon, gX/mean(gX)),
+    stat="smooth",
+    method ="lm", 
+    se=FALSE,
+    size =1) + 
+  geom_line(aes(epsilon, gY/mean(gY)),
+            stat="smooth",
+            method ="lm", 
+            se=FALSE,
+            size =1, colour="green") +
+  my_theme
+
+
+# soical Y
+ggplot(filter(plotDat)) +
+  facet_grid(~epsilon, scales="free") +
+  geom_path(aes(avgR, investGYS,  color=treat), size=1) +
   scale_colour_brewer(palette = "Dark2") +
   my_theme
 
 #asocial Y
-ggplot(filter(plotDat, tn==1)) +
+ggplot(filter(plotDat)) +
   facet_grid(~epsilon, scales="free") +
-  geom_path(aes(avgR, investGY,  color=treat), size=1) +
+  geom_path(aes(avgR, investGYA,  color=treat), size=1) +
   scale_colour_brewer(palette = "Dark2") +
   my_theme
 
-ggplot(plotDat) +
+p2<- ggplot(plotDat) +
   facet_grid(~epsilon, scales="free") +
-  geom_path(aes(avgR, gY,  color=treat), size=1) +
+  geom_path(aes(avgR, SgY,  color=treat), size=1) +
   scale_colour_brewer(palette = "Dark2") +
   my_theme
 
-ggplot(plotDat) +
+p1<- ggplot(plotDat) +
   facet_grid(~epsilon, scales="free") +
-  geom_path(aes(avgR, gX,  color=treat), size=1) +
+  geom_path(aes(avgR, SgX,  color=treat), size=1) +
   scale_colour_brewer(palette = "Dark2") +
   my_theme
-
+grid.arrange(p1, p2, nrow = 1)
 ggplot(plotDat) +
   facet_grid(~epsilon, scales="free") +
   geom_path(aes(avgR, totalG,  color=treat), size=1) +
@@ -79,7 +115,6 @@ ggplot(plotDat) +
   my_theme
 
 fullplotDat <- filter(dat, 
-                      tn==2,
                       d>0, epsilon<20) %>%
   group_by(d,epsilon, stability, envRatio) %>%
   summarise(
@@ -87,34 +122,81 @@ fullplotDat <- filter(dat,
     iX = mean(indAvgX),
     gY = mean(groupAvgY),
     iY = mean(indAvgY),
+    AgX = mean(AgroupAvgX),
+    AiX = mean(AindAvgX),
+    AgY = mean(AgroupAvgY),
+    AiY = mean(AindAvgY),
+    SgX = mean(SgroupAvgX),
+    SiX = mean(SindAvgX),
+    SgY = mean(SgroupAvgY),
+    SiY = mean(SindAvgY),
     avgR = mean(avgR)
   ) %>%
   mutate(
     investGY = gY/(gX+gY),
+    investGYA = AgY/(AgX+AgY),
+    investGYS = SgY/(SgX+SgY),
     investGX = gX/(gX+gY),
     investIY = iY/(iX+iY),
-    totalG = gY+gX
+    totalG = gY+gX,
+    totalGA = (AgX+AgY),
+    totalGA = (SgX+SgY)
   )%>%
   ungroup()
 
+pd2 <- fullplotDat %>%
+  pivot_longer(c(investGYS, investGYA), names_to = "Y_ratio", values_to = "yval")
 
-ggplot(filter(fullplotDat), 
-       aes(log(envRatio), investGY, colour=as.factor(epsilon))) +
-  geom_point() + 
-  geom_line(stat="smooth",
+ggplot(pd2, aes(log(envRatio), yval)) +
+  geom_point(aes(shape=Y_ratio, colour=as.factor(epsilon)), alpha=0.1) +
+  geom_line(aes(linetype=Y_ratio, colour=as.factor(epsilon)),
+            stat="smooth",
             method ="lm", 
             se=FALSE,
-            size =1, alpha=0.4) +
+            size =1) +
   scale_colour_brewer(palette = "Dark2") +
   my_theme
 
-ggplot(filter(fullplotDat), 
-       aes(log(stability), investGY, colour=as.factor(epsilon))) +
-  geom_point() + 
-  geom_line(stat="smooth",
+pd2 <- fullplotDat %>%
+  pivot_longer(c(investGYS, investGYA), names_to = "Y_ratio", values_to = "yval")
+
+ggplot(pd2, aes(log(envRatio), yval)) +
+  geom_point(aes(shape=Y_ratio, colour=as.factor(epsilon)), alpha=0.1) +
+  geom_line(aes(linetype=Y_ratio, colour=as.factor(epsilon)),
+            stat="smooth",
             method ="lm", 
             se=FALSE,
-            size =1, alpha=0.4)+
+            size =1) +
   scale_colour_brewer(palette = "Dark2") +
   my_theme
+
+ggplot(pd2, aes(log(stability), yval)) +
+  geom_point(aes(shape=Y_ratio, colour=as.factor(epsilon)), alpha=0.1) +
+  geom_line(aes(linetype=Y_ratio, colour=as.factor(epsilon)),
+            stat="smooth",
+            method ="lm", 
+            se=FALSE,
+            size =1) +
+  scale_colour_brewer(palette = "Dark2") +
+  my_theme
+
+
+
+# ggplot(filter(fullplotDat)) +
+#    geom_point(aes(log(stability), investGYS, colour=as.factor(epsilon)), 
+#               alpha=0.1, shape=15) +
+#      geom_point(aes(log(stability), investGYA, colour=as.factor(epsilon)), 
+#               alpha=0.1, shape=17) + 
+#      geom_line(aes(log(stability), investGYS, colour=as.factor(epsilon)),
+#                stat="smooth",
+#                method ="lm", 
+#                se=FALSE,
+#                size =1) +
+#      geom_line(aes(log(stability), investGYA, colour=as.factor(epsilon)),
+#                stat="smooth",
+#                method ="lm", 
+#                se=FALSE,
+#                size =1, linetype="dashed") +
+#      scale_colour_brewer(palette = "Dark2") +
+#      my_theme
 
