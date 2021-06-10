7dfd8690004e1fee337fa9a1ab29392dd850cfcc
Mati <matis.patel@gmail.com>
2021-06-10 16:52:50 +0100

    update script

--- code/calc.jl
+++ code/calc.jl
@@ -1,17 +1,17 @@
 function gain(s, h)
-    return(h/s)
+    return(h/(h*s + s))
 end
 
 function loss(s, h)
-    return((1-h)/s)
+    return(1/(h*s + s))
 end
 
 function gl(s, h)
-    return([gain(s,h), loss(s, h)])
+    return([gain(s,h), loss(s, h)])  
 end
 
 s = 2:2:20
-h = 0.1:0.1:0.9
+h = vcat(0.2:0.2:1, 2:2:10)
 
 ls = []
 for s1 in s
