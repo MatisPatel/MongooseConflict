76ceffa3506bd7a6209266970599218352bfcf59
MatisPatel <matis.patel@gmail.com>
2023-10-11 19:12:16 +0100

    change plot code and hpc code

--- code_2023/viperCode/generate_plots.jl
+++ code_2023/viperCode/generate_plots.jl
@@ -5,4 +5,9 @@ using DataFrames
 df = load("processed_df.jld2")["df"]
 
 # filter df for q=3 n=3
-dfplot = filter(df -> df[:q]==3 && df[:n]==3, df)
+dfplot = filter(df -> df[:q]==3 && 
+df[:n]==3 && df[:stab]==2 && df[:shape_X_cost]==2 && df[:shape_Y_cost]==2, df)
+
+plot(dfplot, x=:ratio, y=:meanExprX, color=:epsilon)
+
+plot(dfplot, x=:ratio, y=:meanExprY, color=:epsilon)
