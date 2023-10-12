a45593914ef8363ebce72fecef6b747d5c9e21a3
matis.patel <matis.patel@gmail.com>
2023-10-12 10:09:33 +0100

    hpc full run and update bash to offset indicies

--- code_2023/viperCode/test_batch.sh
+++ code_2023/viperCode/test_batch.sh
@@ -5,7 +5,10 @@
 #SBATCH -e logs/%N.%j.err
 #SBATCH -p compute
 #SBATCH -t 00:20:00
-#SBATCH --array=1-10
+#SBATCH --array=1-1000
+
+# create NEW_TASK_ID which is the SLURM_ARRAY_TASK_ID but offset by a multiplier variable to cover all params
+
 
 module purge
 module add julia 
