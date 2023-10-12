29cf9f77625aaab3b9d9783d4fda9503bf20c3c9
MatisPatel <matis.patel@gmail.com>
2023-10-12 10:14:56 +0100

    added mult to offset

--- code_2023/viperCode/test_batch.sh
+++ code_2023/viperCode/test_batch.sh
@@ -6,12 +6,13 @@
 #SBATCH -p compute
 #SBATCH -t 00:20:00
 #SBATCH --array=1-1000
+mult = 1
 
 # create NEW_TASK_ID which is the SLURM_ARRAY_TASK_ID but offset by a multiplier variable to cover all params
-
+NEW_TASK_ID=$((SLURM_ARRAY_TASK_ID + mult*1000))
 
 module purge
 module add julia 
 
-julia ../code/hpc_run.jl $SLURM_ARRAY_TASK_ID > logs/log.$SLURM_ARRAY_TASK_ID.txt
+julia ../code/hpc_run.jl $NEW_TASK_ID > logs/log.$NEW_TASK_ID.txt
 
