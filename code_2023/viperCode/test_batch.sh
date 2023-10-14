54f77f80119fecb221c421a3a11f16a7d67ed5e0
matis.patel <matis.patel@gmail.com>
2023-10-14 11:56:40 +0100

    makes batch sub have ci arg for mult

--- code_2023/viperCode/test_batch.sh
+++ code_2023/viperCode/test_batch.sh
@@ -4,10 +4,10 @@
 #SBATCH -o logs/%N.%j.out
 #SBATCH -e logs/%N.%j.err
 #SBATCH -p compute
-#SBATCH -t 00:20:00
+#SBATCH -t 00:30:00
 #SBATCH --array=1-1000
 
-mult=4
+mult=$1
 # create NEW_TASK_ID which is the SLURM_ARRAY_TASK_ID but offset by a multiplier variable to cover all params
 NEW_TASK_ID=$( echo ${SLURM_ARRAY_TASK_ID} | awk '{ print $1+('$mult'*1000)}' )
 
