5acb09623634283963777d96c88419be0e22dd1f
matis.patel <matis.patel@gmail.com>
2023-10-14 12:16:01 +0100

    remove empty files

--- code_2023/viperCode/make_database.jl
+++ code_2023/viperCode/make_database.jl
@@ -4,4 +4,13 @@ using DataFrames
 
 datdir = joinpath("..", "data")
 
+files = readdir(datdir)
+
+for file in files 
+    try
+        row = load(joinpath(datdir, file))
+    catch 
+        
+end
+
 df = collect_results!(datdir)
