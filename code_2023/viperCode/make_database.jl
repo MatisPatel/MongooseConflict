d59bf9bc1d253d5bd29194da4d94508993735aac
matis.patel <matis.patel@gmail.com>
2023-10-14 18:25:32 +0100

    making new zip with new sim results

--- code_2023/viperCode/make_database.jl
+++ code_2023/viperCode/make_database.jl
@@ -9,19 +9,14 @@ files = readdir(datdir)
 # loop over files and read if any read fails delete the file
 for file in files
     try
+        println(string("attempting to load file: ", file))
         load(joinpath(datdir, file))
     catch
+        println("Error reading file, deleting file from dir")
         rm(joinpath(datdir, file))
     end
 end
 
 files = readdir(datdir)
-
-for file in files 
-    try
-        row = load(joinpath(datdir, file))
-    catch 
-        
-end
 
 df = collect_results!(datdir)
