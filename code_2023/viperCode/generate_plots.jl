84d035d79bf4a1bdd3ae9006f85badece1c8636d
MatisPatel <matis.patel@gmail.com>
2023-10-16 15:32:05 +0100

    plot script

--- code_2023/viperCode/generate_plots.jl
+++ code_2023/viperCode/generate_plots.jl
@@ -1,31 +1,33 @@
-using PlotlyJS
+using Plots
 using CSVFiles
 using DataFrames
 
 df = load("processed_df.jld2")["df"]
 
+# acc = zeros(first(dfplot.q),first(dfplot.n))
+# n = length(dfplot.tR)
+# for x in dfplot.tR 
+#     acc += x./n
+# end
+# display(acc)
+# println(mean(acc[:, 3:end]))
+
 # filter df for q=3 n=3
 dfplot = filter(df -> 
-    all(df[:tR].>=-0.1) &&
-    df[:q]==5 && 
-    df[:n]==5 && 
-    # df[:epsilon] == 2 &&
-    df[:stab]==2 && 
-    df[:shape_X_cost]==2 && 
-    df[:shape_Y_cost]==2, 
+    all(df[:tR].>=-0.01) &&
+    df[:q]==3 && 
+    df[:n]==3 &&
+    df[:stab] in [1.0, 3.0],
 df)
 
-acc = zeros(first(dfplot.q),first(dfplot.n))
-n = length(dfplot.tR)
-for x in dfplot.tR 
-    acc += x./n
-end
-display(acc)
-println(mean(acc[:, 3:end]))
+# plot of meanX expression vs ratio and save the plot
+plt = plot(dfplot, x=:ratio, y=:meanExprX, color=:epsilon, facet_row=:stab,
+layout=Layout(title = "Mean X expression vs ratio", 
+    xaxis_title = "Ratio", 
+    yaxis_title = "Mean X expression"))
+savefig(plt, "../graphs/3x3_ratio_meanXexpr.png")
 
-plot(dfplot, x=:ratio, y=:meanExprX, color=:q)
-
-plot(dfplot, x=:ratio, y=:meanExprY, color=:q)
+plot(dfplot, x=:ratio, y=:meanExprY, color=:epsilon)
 
 plot(dfplot, x=:ratio, y=:meanR, color=:epsilon)
 
