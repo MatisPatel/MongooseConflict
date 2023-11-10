f56d28c0018a97af9fe7eaa91ece0f211d552e8a
MatisPatel <matis.patel@gmail.com>
2023-11-10 09:23:15 +0000

    add plotting code

---
+++ code_2023/code/plottign_code.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+df <- read.csv2("../viperCode/plotData.csv")
--- code_2023/viperCode/generate_plots.jl
+++ code_2023/viperCode/generate_plots.jl
@@ -1,37 +1,152 @@
-using Plots
+using PlotlyJS
 using CSVFiles
 using DataFrames
 
 df = load("processed_df.jld2")["df"]
-
-# acc = zeros(first(dfplot.q),first(dfplot.n))
-# n = length(dfplot.tR)
-# for x in dfplot.tR 
-#     acc += x./n
-# end
-# display(acc)
-# println(mean(acc[:, 3:end]))
+save("plotData.csv", df)
 
 # filter df for q=3 n=3
 dfplot = filter(df -> 
-    all(df[:tR].>=-0.01) &&
+    # all(df[:tR].>=-0.01) &&
     df[:q]==3 && 
     df[:n]==3 &&
-    df[:stab] in [1.0, 3.0],
+    df[:stab] in [1.0, 3.0] &&
+    df[:epsilon] in [1.0, 2.0, 3.0],
 df)
 
 # plot of meanX expression vs ratio and save the plot
-plt = plot(dfplot, x=:ratio, y=:meanExprX, color=:epsilon, facet_row=:stab,
-layout=Layout(title = "Mean X expression vs ratio", 
-    xaxis_title = "Ratio", 
-    yaxis_title = "Mean X expression"))
+plt = plot(dfplot, x=:harshness, y=:meanExprX, color=:epsilon, facet_row=:stab);
 savefig(plt, "../graphs/3x3_ratio_meanXexpr.png")
 
-plot(dfplot, x=:ratio, y=:meanExprY, color=:epsilon)
+# plot of meanY expression vs harshness and save the plot 
+plt = plot(dfplot, x=:harshness, y=:meanExprY, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/3x3_ratio_meanYexpr.png")
 
-plot(dfplot, x=:ratio, y=:meanR, color=:epsilon)
+# plot of meanR vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanR, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/3x3_ratio_meanR.png")
 
-plot(dfplot, x=:ratio, y=:popSize, color=:epsilon)
+# plot of meanW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/3x3_ratio_meanW.png")
 
-# PlotlyJS plot histogram of :timings 
-plot(dfplot, x=:timing, color=:epsilon, kind=:histogram)
+# plot of varW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:varW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/3x3_ratio_varW.png")
+
+# filter df for q=4 n=4
+dfplot = filter(df -> 
+    all(df[:tR].>=-0.01) &&
+    df[:q]==4 && 
+    df[:n]==4 &&
+    df[:stab] in [1.0, 3.0] &&
+    df[:epsilon] in [1.0, 2.0, 3.0],
+df)
+
+# plot of meanX expression vs ratio and save the plot
+plt = plot(dfplot, x=:harshness, y=:meanExprX, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x4_ratio_meanXexpr.png")
+
+# plot of meanY expression vs harshness and save the plot 
+plt = plot(dfplot, x=:harshness, y=:meanExprY, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x4_ratio_meanYexpr.png")
+
+# plot of meanR vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanR, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x4_ratio_meanR.png")
+
+# plot of meanW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x4_ratio_meanW.png")
+
+# plot of varW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:varW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x4_ratio_varW.png")
+
+# filter df for q=5 n=5
+dfplot = filter(df -> 
+    all(df[:tR].>=-0.01) &&
+    df[:q]==5 && 
+    df[:n]==5 &&
+    df[:stab] in [1.0, 3.0] &&
+    df[:epsilon] in [1.0, 2.0, 3.0],
+df)
+
+# plot of meanX expression vs ratio and save the plot
+plt = plot(dfplot, x=:harshness, y=:meanExprX, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/5x5_ratio_meanXexpr.png")
+
+# plot of meanY expression vs harshness and save the plot 
+plt = plot(dfplot, x=:harshness, y=:meanExprY, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/5x5_ratio_meanYexpr.png")
+
+# plot of meanR vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanR, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/5x5_ratio_meanR.png")
+
+# plot of meanW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/5x5_ratio_meanW.png")
+
+# plot of varW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:varW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/5x5_ratio_varW.png")
+
+# filter df for q=6 n=4
+dfplot = filter(df -> 
+    all(df[:tR].>=-0.01) &&
+    df[:q]==6 && 
+    df[:n]==4 &&
+    df[:stab] in [1.0, 3.0] &&
+    df[:epsilon] in [1.0, 2.0, 3.0],
+df)
+
+# plot of meanX expression vs ratio and save the plot
+plt = plot(dfplot, x=:harshness, y=:meanExprX, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/6x4_ratio_meanXexpr.png")
+
+# plot of meanY expression vs harshness and save the plot 
+plt = plot(dfplot, x=:harshness, y=:meanExprY, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/6x4_ratio_meanYexpr.png")
+
+# plot of meanR vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanR, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/6x4_ratio_meanR.png")
+
+# plot of meanW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/6x4_ratio_meanW.png")
+
+# plot of varW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:varW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/6x4_ratio_varW.png")
+
+# filter df for q=4 n=6
+dfplot = filter(df -> 
+    all(df[:tR].>=-0.01) &&
+    df[:q]==4 && 
+    df[:n]==6 &&
+    df[:stab] in [1.0, 3.0] &&
+    df[:epsilon] in [1.0, 2.0, 3.0],
+df)
+
+# plot of meanX expression vs ratio and save the plot
+plt = plot(dfplot, x=:harshness, y=:meanExprX, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x6_ratio_meanXexpr.png")
+
+# plot of meanY expression vs harshness and save the plot 
+plt = plot(dfplot, x=:harshness, y=:meanExprY, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x6_ratio_meanYexpr.png")
+
+# plot of meanR vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanR, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x6_ratio_meanR.png")
+
+# plot of meanW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:meanW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x6_ratio_meanW.png")
+
+# plot of varW vs harshness and save plot 
+plt = plot(dfplot, x=:harshness, y=:varW, color=:epsilon, facet_row=:stab);
+savefig(plt, "../graphs/4x6_ratio_varW.png")
+
