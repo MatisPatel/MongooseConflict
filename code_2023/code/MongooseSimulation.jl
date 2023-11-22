ff298d48e79f2c7fa8f1cfba96f03324b9048338
MatisPatel <matis.patel@gmail.com>
2023-11-22 14:00:34 +0000

    add functions to sweep ratios

--- code_2023/code/MongooseSimulation.jl
+++ code_2023/code/MongooseSimulation.jl
@@ -605,7 +605,7 @@ function makeWsys(W, F, Mf, Ml, P, Pf, Pl, C, Cf, Cl, d, epsilon, world)
     return wSys, Wn6, Wd6 
 end
 
-function makeRsys(R, M, P, C, d, world)
+function makeRsys(R, F, M, P, C, d, world)
     Rp = makeFArray(world)
     Rp1 = addTransR(copy(Rp), R, F, world)
     Rp2 = addMortalityR(copy(Rp1), R, F, M, world)
@@ -763,7 +763,7 @@ function genSolR(rFun, world)
                 (world[:q], world[:n])
             )
         )
-    )
+    , autodiff = :forward, method=:anderson)
 end
 
 function corrErr(g, t)
@@ -1057,7 +1057,7 @@ function runSim(world)
         );
     callFun = eval(funF[2]);
 
-    rSys = Symbolics.scalarize(makeRsys(R, M, P, C, d, world));
+    rSys = Symbolics.scalarize(makeRsys(R, F, M, P, C, d, world));
     # R2 = collect(Symbolics.value.(R));
     # [R2[p, 1] = 0.0 for p in 1:world[:q]];
     # [R2[p, 2] = 0.0 for p in 1:world[:q]];
@@ -1250,6 +1250,192 @@ end
 function RMSprop_update(learning_rate, cache1, cache2, grad, decay)
     cache1 = decay .* cache1 .+ (1 - decay) .* clamp.(grad, -1, 1).^2
     return grad .* (learning_rate ./ ((sqrt.(0.5.*(cache1 .+ mean(cache2)))) .+ 1E-8)), cache1
+end
+
+function re_evaluate_world(world, ratio)
+
+    # println("Defining Vars ")
+    # println("q: ", world[:q]) 
+    # println("n: ", world[:n])
+
+    evolved_X = copy(world[:tX])
+    evolved_Y = copy(world[:tY])
+    evolved_R = copy(world[:tR])
+    evolved_W = copy(world[:tW])
+    evolved_ratio = copy(world[:ratio])
+
+    Symbolics.@variables begin 
+        F[1:world[:q], 1:world[:n]]
+        W[1:world[:q], 1:world[:n]]
+        R[1:world[:q], 1:world[:n]]
+        M[1:world[:q], 1:world[:n]]
+        Mf[1:world[:q], 1:world[:n]]
+        Ml[1:world[:q], 1:world[:n]]
+        P[1:world[:q], 1:world[:n]]
+        Pf[1:world[:q], 1:world[:n]]
+        Pl[1:world[:q], 1:world[:n]]
+        C[1:world[:q], 1:world[:n]]
+        Cf[1:world[:q], 1:world[:n]]
+        Cl[1:world[:q], 1:world[:n]]
+        Tr[1:world[:q], 1:world[:q]]
+        X[1:world[:q], 1:world[:n]]
+        Y[1:world[:q], 1:world[:n]]
+        Xf[1:world[:q], 1:world[:n]]
+        Yf[1:world[:q], 1:world[:n]]
+        Xl[1:world[:q], 1:world[:n]]
+        Yl[1:world[:q], 1:world[:n]]
+    end
+    # update world [:ratio] and rebuild equations and resolve them for tF, tW and tR 
+    world[:ratio] = ratio
+    world[:gain] = world[:ratio]/world[:stab]
+    world[:loss] = (1-world[:ratio])/world[:stab]
+    fSys = makeFsys(F, M, P, C, d, world)
+
+    fSys = makeFsys(F, M, P, C, d, world)
+    funF = build_function(
+        fSys, F, M, P, C, d, epsilon;
+        expression=Val{false}
+        );
+    callFun = eval(funF[2]);
+
+    rSys = Symbolics.scalarize(makeRsys(R, F, M, P, C, d, world));
+    rSys = substitute(rSys, Dict(vcat([R[p, 1]=> 0 for p in 1:world[:q]], [R[p, 2]=> 0 for p in 1:world[:q]])))
+
+    funR = build_function(
+        rSys, R, F, M, P, C, d, epsilon;
+        expression=Val{false}
+        );
+    callFunR = eval(funR[2]);
+
+    wSys, Wn, Wd = makeWsys(W, F, Mf, Ml, P, Pf, Pl, C, Cf, Cl, d, epsilon, world);
+    # set the non inhabited patches to 0 
+    for q in 1:world[:q]
+        wSys[q, 1] = W[q,1]
+    end
+    # set the fixed patches to the average fitness 
+    numInds = reshape(repeat([x-1 for x in 1:world[:n]], world[:q]), (world[:n],world[:q]))'
+    # wSys[1, 2] = 1-Symbolics.scalarize(sum(W.*F.*numInds)/sum(F.*numInds))
+    # wSys = substitute(Symbolics.scalarize(wSys), Dict(W[p, 1]=> 0 for p in 1:world[:q]))
+    wSys[1,2] = 1 - sum(W)
+
+    wSysSelec = Symbolics.scalarize(Wn./Wd);
+    funW = build_function(
+        wSys, W, F, Mf, Ml, Pf, Pl, P, C, Cl, Cf, d, epsilon;
+        expression=Val{false}
+        );
+    callFunW = eval(funW[2]);
+
+
+    fFun(Fx, x) = callFun(
+        reshape(Fx, (world[:q], world[:n])), 
+        reshape(x, (world[:q], world[:n])),  
+        world[:M], 
+        world[:P], 
+        world[:C], 
+        world[:d], 
+        world[:epsilon]
+        )
+
+    rFun(Fx, x) = callFunR(
+        reshape(Fx, (world[:q], world[:n])), 
+        reshape(x, (world[:q], world[:n])), 
+        solF.zero, 
+        world[:M], 
+        world[:P], 
+        world[:C], 
+        world[:d], 
+        world[:epsilon]
+        )
+
+    wFun(Fx, x) = callFunW(
+        reshape(Fx, (world[:q], world[:n])), 
+        reshape(x, (world[:q], world[:n])), 
+        solF.zero, 
+        world[:Mf], 
+        world[:Ml], 
+        world[:Pf], 
+        world[:Pl],
+        world[:P],
+        world[:C],
+        world[:Cl],
+        world[:Cf],
+        world[:d], 
+        world[:epsilon]
+        )
+
+    solF = zeros(world[:q], world[:n])
+    try 
+        solF = genSolF2(fFun, world)
+    catch
+        try 
+            solF = genSolF(fFun, world)  
+        catch
+            solF = zeros(world[:q], world[:n])
+        end
+    end
+
+    solR = zeros(world[:q], world[:n])
+    try 
+        solR = genSolR2(rFun, world)
+    catch
+        try
+            solR = genSolR(rFun, world)  
+        catch
+            solR = zeros(world[:q], world[:n])
+        end
+    end
+
+    solW = zeros(world[:q], world[:n])
+    try 
+        solW = genSolW2(wFun, world)
+    catch
+        try
+            solW = genSolW(wFun, world)  
+        catch 
+            solW = zeros(world[:q], world[:n])
+        end
+    end
+
+    # println("ratio: ", ratio)
+    # println("evolved F: ")
+    # display(world[:tF]) 
+    # println("displaced F: ")
+    # display(solF.zero)
+    try 
+        world[Symbol("tF", "_", ratio)] = solF.zero
+    catch 
+        world[Symbol("tF", "_", ratio)] = solF
+    end
+
+    # println("evolved R: ") 
+    # display(world[:tR])
+    # println("displaced R: ")
+    # display(solR.zero)
+
+    try
+        world[Symbol("tR", "_", ratio)] = solR.zero
+    catch
+        world[Symbol("tR", "_", ratio)] = solR
+    end
+
+    # println("evolved W: ")
+    # display(world[:tW])
+    # println("displaced W: ")
+    # display(solW.zero)
+    try 
+        world[Symbol("tW", "_", ratio)] = solW.zero
+    catch
+        world[Symbol("tW", "_", ratio)] = solW
+    end
+
+    return world
+end
+
+function re_evaluate_world_on_ratios(world, range)
+    for ratio in range
+        world = re_evaluate_world(world, ratio)
+    end
+    return world
 end
 
 # end
